b4173e3abc73e2a506aa3a5183ca7a93d191fa39
renan silva da costa <renandev.rj@gmail.com>
2026-03-12 20:02:58 -0300

    create UserController

--- src/main/java/com/RenanSCosta/todosimple/models/User.java
+++ src/main/java/com/RenanSCosta/todosimple/models/User.java
@@ -1,6 +1,7 @@
 package com.RenanSCosta.todosimple.models;
 
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonProperty.Access;
 import jakarta.persistence.*;
@@ -13,15 +14,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 
-@Getter
-@Setter
 @EqualsAndHashCode
 @Entity
 @Table(name = "users")
 public class User {
 
-    public interface createUser {}
-    public interface updateUser {}
+    public interface CreateUser {}
+    public interface UpdateUser {}
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -29,16 +28,16 @@ public class User {
     private Long id;
 
     @Column(name = "username", length = 100, nullable = false, unique = true)
-    @NotNull(groups = createUser.class)
-    @NotEmpty(groups = createUser.class)
-    @Size(groups = createUser.class, min = 3, max = 100)
+    @NotNull(groups = CreateUser.class)
+    @NotEmpty(groups = CreateUser.class)
+    @Size(groups = CreateUser.class, min = 3, max = 100)
     private String username;
 
     @JsonProperty(access = Access.WRITE_ONLY) // impede que a senha seja exposta em respostas JSON
     @Column(name = "password", length = 255, nullable = false)
-    @NotNull(groups = {createUser.class, updateUser.class})
-    @NotEmpty (groups = {createUser.class, updateUser.class})
-    @Size(groups = {createUser.class, updateUser.class}, min = 8, max = 255)
+    @NotNull(groups = {CreateUser.class, UpdateUser.class})
+    @NotEmpty (groups = {CreateUser.class, UpdateUser.class})
+    @Size(groups = {CreateUser.class, UpdateUser.class}, min = 3, max = 255)
     private String password;
 
     @OneToMany(mappedBy = "user")
@@ -53,4 +52,37 @@ public class User {
     }
 
 
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @JsonIgnore
+    public List<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(List<Task> tasks) {
+        this.tasks = tasks;
+    }
 }
